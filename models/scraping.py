0bea624ecbb2e0f5fbfcb70af9a6603d6c2aa381
shoga <187181800+shoga900@users.noreply.github.com>
2024-11-18 17:07:48 +0900

    modifying

--- models/scraping.py
+++ models/scraping.py
@@ -5,49 +5,41 @@ import time
 
 from models import handling
 
-class Robot(object):
-    """Base model for Robot."""
-    def __init__(self,speak_color='green'):
-        self.speak_color = speak_color
+class Scraping(object):
+    def __init__(self,code='9252'): 
+        # base: 9252
+        # haishi: 5127
+        self.code = code
 
-class ScrapingRobot(Robot):
+class ScrapingRobot(Scraping):
     """Scrape company information"""
 
     def read_company_list(self):
         print('------------------------')
         print(f'input list')
         print('------------------------')
-    
 
-    def scrape_company(self):
-
-        code = '9252' # Temp
-
-
+    def scrape_basic_info(self):
         # 現在の株価と発行済株式数を抽出する
-        URL = 'https://kabutan.jp/stock/?code=' + str(code)
+        URL = 'https://kabutan.jp/stock/?code=' + str(self.code)
 
         try:
             # 現在の株価と発行済株式数を抽出する
             dfs = pd.read_html(URL, match='発行済株式')
 
             df = dfs[0]
-            temp = df.iat[9, 1]
-            now_stocks = int(temp[:-2].replace(',', ''))
-            time.sleep(0.2)
+            market_capitalization = df.iat[8, 1][:-2]
+            temp_now_stocks = df.iat[9, 1]
+            now_stocks = int(temp_now_stocks[:-2].replace(',', ''))
 
-            dfs = pd.read_html(URL, match='始値')
-            df = dfs[0]
-            now_price = df.iat[3, 1]
-
-            # for df in dfs:
-            #     print(df)
-            #     print("---------------")
-
-            res_basic = {
-                'now_stocks': now_stocks,
-                'now_price': now_price,
+            result = {
+                '時価総額(億円)': market_capitalization,
+                '株式数(株)': now_stocks,
                 }
+            
+            result_df = pd.DataFrame([result])
+            print(result_df)
+
             time.sleep(0.5)
 
             # return res_basic
@@ -56,11 +48,12 @@ class ScrapingRobot(Robot):
             # テキストが含まれていない場合の処理
             print("'ページが見つかりませんでした。処理をスキップします。")
             return
+    
 
-
+    def scrape_stock_price(self):
         # 株価を月足で取得し、倍率を出力する
         URL = 'https://kabutan.jp/stock/kabuka?code=' \
-          + str(code) + '&ashi=mon'
+          + str(self.code) + '&ashi=mon'
 
         dfs = pd.read_html(URL, match='始値')
         df = dfs[1]
@@ -97,8 +90,10 @@ class ScrapingRobot(Robot):
                 "期間(年)": years_diff,
                 "倍率(倍)": multiplier,
             }
+
             result_df = pd.DataFrame([result])
             print(result_df)
+
         else:
             print("基準月以降にデータがありません。")
             result = {
@@ -106,12 +101,65 @@ class ScrapingRobot(Robot):
                 "基準終値": base_close,
             }
 
+        time.sleep(0.5)
 
-        # 年単位の業績を取得する
 
+    def scrape_sales(self):
+        # 決算ページから業績データを取得する
+        URL = 'https://kabutan.jp/stock/finance?code=' \
+            + str(self.code)
 
-        # aaa
+        # 過去の決算数値をスクレイピング
+        dfs = pd.read_html(URL, match='決算期')
 
+        if len(dfs[0]) < 8: # 4年前までの株価データがない場合はブレイクする
+            return {}
+
+        df = dfs[0]
+        df = df.drop(columns=['修正1株配', '発表日'])
+        df = df.dropna(how='all')
+        df = df.drop(len(df))
+        df = df.iloc[::-1]  # 時系列降順に並び替え
+        df.index = range(len(df))
+
+        # 1年前の決算情報を抽出する
+        past1_year = df.iat[1, 0]
+        past1_sales = df.iat[1, 1]
+        past1_OP = df.iat[1, 2]
+        past1_EPS = df.iat[1, 5]
+
+        # 2年前の決算情報を抽出する
+        past2_year = df.iat[2, 0]
+        past2_sales = df.iat[2, 1]
+        past2_OP = df.iat[2, 2]
+        past2_EPS = df.iat[2, 5]
+
+        # 3年前の決算情報を抽出する
+        past3_year = df.iat[3, 0]
+        past3_sales = df.iat[3, 1]
+        past3_OP = df.iat[3, 2]
+        past3_EPS = df.iat[3, 5]
+
+        result = {
+            'sales3_year': past3_year,
+            'sales3_sales': past3_sales,
+            'sales3_OP': past3_OP,
+            'sales3_EPS': past3_EPS,
+            'sales2_year': past2_year,
+            'sales2_sales': past2_sales,
+            'sales2_OP': past2_OP,
+            'sales2_EPS': past2_EPS,
+            'sales1_year': past1_year,
+            'sales1_sales': past1_sales,
+            'sales1_OP': past1_OP,
+            'sales1_EPS': past1_EPS,
+        }
+
+        print(result)
+
+        time.sleep(0.5)
+
+        # return res_sales
 
 
 
